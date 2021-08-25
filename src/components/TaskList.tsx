2b99881cfe4ed78ebb606fc76d4bd188e9e38a45
Junior Camargos <camargosjr@gmail.com>
2021-08-25 08:28:44 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return
+    const newTask ={
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const changeTask = tasks.map(function(task){
+     if(task.id === id){
+      task.isComplete = !task.isComplete;
+     }
+     return task;
+    });
+    setTasks(changeTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(function(task){
+      return task.id != id;
+    });
+    setTasks(removeTask);
   }
 
   return (
